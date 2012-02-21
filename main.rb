4df87ec50434af0d06f16b51a89bb66904659347
n-jax <n@j.com>
2012-02-21 05:57:15 -0600

    made a new function for obliterating duplicators from the tally

--- main.rb
+++ main.rb
@@ -14,10 +14,26 @@ def karel(inputs)
   (numbers-duplicates).inject(:+)
 end
 
-def notary(inputs)
-  numbers = inputs
+def notary(*inputs)
+  numbers = *inputs
   numbers.uniq!
   numbers.inject(:+)
+end
+
+def notary_obli_dup(numbers)
+  unique = numbers.uniq
+  duplicators = subtract_n_from_u(numbers, unique)
+  duplicators.each do |d|
+    unique.delete(d)
+  end
+  unique.inject(:+)
+end
+
+def subtract_n_from_u(numbers, unique)
+  unique.each do |u|
+    numbers.slice!(numbers.index(u))
+  end
+  return numbers # duplicators
 end
 
 def test_karel(numbers=[1,2,4,5,7,1,2,4,1,5,6])
@@ -30,20 +46,30 @@ def test_karel(numbers=[1,2,4,5,7,1,2,4,1,5,6])
   time
 end
 
-def test_notary(numbers=[1,2,4,5,7,1,2,4,1,5,6])
+def test_notary(*numbers)
   time = Benchmark.measure do
     2000000.times do
-      numbers = numbers
+      numbers = *numbers
       notary(numbers)
     end
   end
   time
 end
 
-example_array = [1,2]
+def test_notary_obli_dup(numbers)
+  time = Benchmark.measure do
+    2000000.times do
+      numbers = numbers
+      notary_obli_dup(numbers)
+    end
+  end
+  time
+end
+
+example_array = [1,2,2,3]
 
 puts "Speed for ruby sort:"
 puts test_karel example_array
 
-puts "Speed for ruby unique:"
-puts test_notary example_array
+puts "Speed for ruby unique obliterate:"
+puts test_notary_obli_dup example_array
